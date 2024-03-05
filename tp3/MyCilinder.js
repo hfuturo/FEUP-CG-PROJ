6476c1a72593609cadbdef4b7ee56c5ee29ece26
Tomás Xavier <up202108759@edu.fe.up.pt>
2024-03-05 09:45:38 +0000

    fixed cilinder

--- tp3/MyCilinder.js
+++ tp3/MyCilinder.js
@@ -29,14 +29,15 @@ export class MyCilinder extends CGFobject {
 
 		for(var i = 0; i < this.slices; i++){
 			var step = 1/this.stacks;
+			this.vertices.push(Math.cos(ang),-Math.sin(ang), 0);
+			this.normals.push(Math.cos(ang), -Math.sin(ang), 0);
 			for (var j = 0; j < this.stacks; j ++){
-				this.vertices.push(Math.cos(ang),-Math.sin(ang), j*step);
 				this.vertices.push(Math.cos(ang),-Math.sin(ang), (j+1)*step);
-				this.indices.push(i*this.stacks*2+j*2, ((i*this.stacks*2+j*2)+this.stacks*2+1) % (this.slices*this.stacks*2), ((i*this.stacks*2+j*2)+this.stacks*2) % (this.slices*this.stacks*2));
-				this.indices.push(i*this.stacks*2+j*2, ((i*this.stacks*2+j*2)+1) % (this.slices*this.stacks*2), ((i*this.stacks*2+j*2)+this.stacks*2+1) % (this.slices*this.stacks*2));
-				this.normals.push(Math.cos(ang), -Math.sin(ang), 0);
+				this.indices.push(i*(this.stacks+1)+j, ((i*(this.stacks+1)+j)+this.stacks+2) % (this.slices*(this.stacks+1)), ((i*(this.stacks+1)+j)+this.stacks+1) % (this.slices*(this.stacks+1)));
+				this.indices.push(i*(this.stacks+1)+j, ((i*(this.stacks+1)+j)+1) % (this.slices*(this.stacks+1)), ((i*(this.stacks+1)+j)+this.stacks+2) % (this.slices*(this.stacks+1)));
 				this.normals.push(Math.cos(ang), -Math.sin(ang), 0);
 			}
+			//this.vertices.pop();
 			ang+=alphaAng;
 		}
 
