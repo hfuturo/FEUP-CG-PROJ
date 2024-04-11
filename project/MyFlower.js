f025f2f0d7e56f88caefcbddd208b6a4a05736be
Henrique Silva <up202105647@edu.fe.up.pt>
2024-04-11 12:13:33 +0100

    flower has random colors

--- project/MyFlower.js
+++ project/MyFlower.js
@@ -19,28 +19,35 @@ export class MyFlower extends CGFobject {
 
         this.numberOfPetals = numberOfPetals;
 
+        this.petalColors = ["#ff7b00", "#ff8800", "#ff9500", "#ffa200", "#ffaa00", "#ffb700", "#ffc300", "#ffd000", "#ffdd00", "#ffea00"];
+        this.petalColor = this.getRandomColor(this.petalColors);
+        this.receptacleColors = ["#ede0d4", "#e6ccb2", "#ddb892", "#b08968", "#7f5539", "#9c6644"];
+        this.receptacleColor = this.getRandomColor(this.receptacleColors);
+        this.stemColors = ["#004b23", "#006400", "#007200", "#008000", "#38b000", "#70e000", "#9ef01a", "#ccff33"];
+        this.stemColor = this.getRandomColor(this.stemColors);
+
 		this.initBuffers();
 	}
 
 	display() {
 
         const deg2rad = Math.PI/180.0;
-
+        
         // stem
         this.scene.pushMatrix();
         this.scene.scale(0.2, 5, 0.2);
-        this.scene.setAmbient(1/255, 50/255, 32/255, 1);
-        this.scene.setDiffuse(1/255, 50/255, 32/255, 1);
-        this.scene.setSpecular(1/255, 50/255, 32/255, 1);
+        this.scene.setAmbient(...this.stemColor);
+        this.scene.setDiffuse(...this.stemColor);
+        this.scene.setSpecular(...this.stemColor);
         this.stem.display();
         this.scene.popMatrix();
 
         // receptacle
         this.scene.pushMatrix();
         this.scene.translate(0, 5, 0);
-        this.scene.setAmbient(196/255, 164/255, 132/255, 1);
-        this.scene.setDiffuse(196/255, 164/255, 132/255, 1);
-        this.scene.setSpecular(196/255, 164/255, 132/255, 1);
+        this.scene.setAmbient(...this.receptacleColor);
+        this.scene.setDiffuse(...this.receptacleColor);
+        this.scene.setSpecular(...this.receptacleColor);
         this.receptacle.display();
         this.scene.popMatrix();
 
@@ -57,9 +64,9 @@ export class MyFlower extends CGFobject {
             this.scene.translate(0, 10, 0);
             this.scene.rotate(angleRad, 0, 0, 1);
             this.scene.translate(0, 1, 0);
-            this.scene.setAmbient(255/255, 215/255, 0/255, 1);
-            this.scene.setDiffuse(255/255, 215/255, 0/255, 1);
-            this.scene.setSpecular(255/255, 215/255, 0/255, 1);
+            this.scene.setAmbient(...this.petalColor);
+            this.scene.setDiffuse(...this.petalColor);
+            this.scene.setSpecular(...this.petalColor);
             this.petal.display();
 
             this.scene.popMatrix();
@@ -87,5 +94,31 @@ export class MyFlower extends CGFobject {
 		this.initBuffers();
 		this.initNormalVizBuffers();
 	}
+
+    hexToRgbA(hex)
+    {
+        let ret;
+        //either we receive a html/css color or a RGB vector
+        if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+            ret=[
+                parseInt(hex.substring(1,3),16).toPrecision()/255.0,
+                parseInt(hex.substring(3,5),16).toPrecision()/255.0,
+                parseInt(hex.substring(5,7),16).toPrecision()/255.0,
+                1.0
+            ];
+        }
+        else
+            ret=[
+                hex[0].toPrecision()/255.0,
+                hex[1].toPrecision()/255.0,
+                hex[2].toPrecision()/255.0,
+                1.0
+            ];
+        return ret;
+    }
+
+    getRandomColor(colors) {
+        return this.hexToRgbA(colors[parseInt(Math.random() * (colors.length - 1))]);
+    }
 }
 
